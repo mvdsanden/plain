d8521b2e48bf74f355c2905c572e7139f4f0f0a7
Marten van de Sanden <mart.vd.sanden@gmail.com>
2015-08-06 19:42:27 -0400

    More comments.

--- src/core/application.h
+++ src/core/application.h
@@ -3,13 +3,25 @@
 
 namespace plain {
 
+  /**
+   *  The application framework.
+   */
   class Application {
   public:
 
+    /**
+     *  This is called with the application is initialized.
+     */
     virtual void create(int argc, char *argv[]) {}
 
+    /**
+     *  This is called right before the application ends.
+     */
     virtual void destroy() {}
 
+    /**
+     *  This is called between event handler updates.
+     */
     virtual void idle() {}
 
   };
